24b31b25eb63cd246cb7c08c3e6be3b6833d968f
raiyan1999haque <raiyan2000haque@gmail.com>
2026-04-16 14:38:49 +0900

    Final working version with URL support

--- app.py
+++ app.py
@@ -69,5 +69,5 @@ def add_quote(book_id):
 
 if __name__ == '__main__':
     # Use environment port for Render
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
